b3e03353974600238a5fb18752eabfdec67a2f96
sammd29 <40930379+sammd29@users.noreply.github.com>
2019-08-24 14:50:26 -0700

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,26 +1,12 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
-
- */
-require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler();
-
-$validator = $pp->getValidator();
-$validator->fields(['firstname','lastname', 'email','phone'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('message')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('ashwansameer@gmail.com'); // ← Your email here
-
-echo $pp->process($_POST);
+<?php 
+ if (isset($_POST['submit'])) {
+ 	$name = $_POST['name'];
+ 	$subject = $_POST['subject'];
+ 	$mailFrom = $_POST['mail'];
+ 	$message = $_POST['message'];
+ 	$mailTo = "smudronja.sm@gmail.com";
+ 	$headers = "From: ".$mailFrom;
+ 	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+ 	mail($mailTo, $subject, $txt, $headers);
+ 	header("Location; index.php?mailsend");
+ }
