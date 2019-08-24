ac79484e64e0d70537e3ee93a7ee8c0237af4f61
sammd29 <40930379+sammd29@users.noreply.github.com>
2019-08-24 14:55:43 -0700

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -4,7 +4,7 @@
  	$subject = $_POST['subject'];
  	$mailFrom = $_POST['mail'];
  	$message = $_POST['message'];
- 	$mailTo = "smudronja.sm@gmail.com";
+ 	$mailTo = "ashwansameer@gmail.com";
  	$headers = "From: ".$mailFrom;
  	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
  	mail($mailTo, $subject, $txt, $headers);
