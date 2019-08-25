84087caf3764d7ad80c259f317e49bf8f3a071af
sammd29 <40930379+sammd29@users.noreply.github.com>
2019-08-25 03:39:21 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,52 @@
-(function ($) {
-    'use strict';
-    var form = $('.contact__form'),
-        message = $('.contact__msg'),
-        form_data;
-    // Success function
-    function done_func(response) {
-        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
-        message.text(response);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-        form.find('input:not([type="submit"]), textarea').val('');
-    }
-    // fail function
-    function fail_func(data) {
-        message.fadeIn().removeClass('alert-success').addClass('alert-success');
-        message.text(data.responseText);
-        setTimeout(function () {
-            message.fadeOut();
-        }, 2000);
-    }
-    
-    form.submit(function (e) {
-        e.preventDefault();
-        form_data = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: form.attr('action'),
-            data: form_data
-        })
-        .done(done_func)
-        .fail(fail_func);
-    });
-    
-})(jQuery);
+<script type="text/javascript">
+$("#my_form").submit(function(event){
+	event.preventDefault(); //prevent default action 
+	var proceed = true;
+	var form = this;
+	
+	//simple validation at client's end
+	//loop through each field and we simply change border color to red for invalid fields		
+	$(form).find(':required').each(function(){
+		$(this).css('border-color',''); 
+		if(!$.trim($(this).val())){ //if this field is empty 
+			$(this).css('border-color','red'); //change border color to red   
+			proceed = false; //set do not proceed flag
+		}
+		//check invalid email
+		var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+		if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+			$(this).css('border-color','red'); //change border color to red   
+			proceed = false; //set do not proceed flag				
+		}	
+
+	}).keyup(function() { //reset previously set border colors on .keyup()
+		$(this).css('border-color',''); 
+	}).change(function() {  //for select box
+		$(this).css('border-color',''); 
+	});	
+	
+	if(proceed){ //everything looks good! proceed...
+		//get input field values data to be sent to server
+		var post_url = $(this).attr("action"); //get form action url
+		var request_method = $(this).attr("method"); //get form GET/POST method
+		var form_data = $(this).serialize(); //Encode form elements for submission
+		
+		//Ajax post data to server
+		$.ajax({
+			url : post_url,
+			type: request_method,
+			dataType : 'json',
+			data : form_data
+		})
+		.done(function(response){ 
+			if(response.type == 'error'){ //load json data from server and output message     
+				output = '<div class="error">'+response.text+'</div>';
+			}else{
+				$(form)[0].reset(); //reset this form upon success
+				output = '<div class="success">'+response.text+'</div>';
+			}
+			$("#contact_form #contact_results").html(output);
+		});
+	}
+});
+</script>
